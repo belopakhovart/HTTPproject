a9014f3c770b06c7dab3c6ca44bad1c8a643391c
Belopakhov Artem <Belopakhovart@gmail.com>
2020-02-26 20:06:08 +0400

    Задача Изображение Марса. Запускать файл test.

--- test.py
+++ test.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 @app.route('/')
-@app.route('/choice/<planet_name>')
-def index(planet_name):
-    return render_template('index.html', planet_name=planet_name)
+@app.route('/image_mars')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
