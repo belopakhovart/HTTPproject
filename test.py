a436f0aebd976f716bbf137982d761a7f998c09c
Belopakhov Artem <Belopakhovart@gmail.com>
2020-02-26 20:17:22 +0400

    Задача Результат отбора. Запускать файл test.

--- test.py
+++ test.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 @app.route('/')
-@app.route('/image_mars')
-def index():
-    return render_template('index.html')
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def index(nickname, level, rating):
+    return render_template('index.html', nickname=nickname, level=level, rating=rating)
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
